6ae5f2d7598c1193c7fdc817cad27e3429bfca67
inline-arc <atharvmore20@gmail.com>
2025-03-09 15:45:39 +0530

    v0.0.7

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import QRCodeDisplay from './components/qr-code'
 import VerificationPlatform from './components/providers'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   
 
   return (
     <>
+    <Analytics/>
       <VerificationPlatform />
     </>
   )
